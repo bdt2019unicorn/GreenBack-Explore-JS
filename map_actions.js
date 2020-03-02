8bc4e05ccd53d24be66169a863d77ddc9d61ffe0
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-02 20:21:13 +1300

    Update map_actions.js

--- map_actions.js
+++ map_actions.js
@@ -95,6 +95,7 @@ function Directions(button)
             var points = [current_location_graphic, point_location_graphic]; 
             support_functions.RemoveIrrelevantPoints(points); 
             support_functions.GetDirection(points); 
+            button.disabled = true; 
             var btn = document.createElement("button"); 
             btn.innerText = "click me "; 
             window.map_view.ui.add(btn, "bottom-left"); 
