78f4becff05bb14cecc258b252395360178a27a5
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-03 10:29:35 +1300

    Update distance_between_2_points.js

--- distance_between_2_points.js
+++ distance_between_2_points.js
@@ -1,33 +1,31 @@
-onmessage = function(event)
-{
-    var post_data = event.data; 
-    post_data = ConvertObject(post_data); 
-    var distance = Distance(post_data); 
-    this.postMessage(distance); 
-}
-
-
 function Distance(points_object)
 {
-    return 3963.0 * 
-        Math.acos
-        (
-            Math.sin(points_object.current_location.latitude) * Math.sin(points_object.destination.latitude) + Math.cos(points_object.current_location.latitude) * Math.cos(points_object.destination.latitude) * Math.cos(points_object.destination.longtitude - points_object.current_location.longtitude)
-        ); 
-}
+    var support_functions = 
+    {
+        ConvertObject(points_object)
+        {
+            var ChangeToRadian = function(number)
+            {
+                return Math.PI * number / 180; 
+            }
+            points_object.current_location.longtitude = ChangeToRadian(points_object.current_location.longtitude); 
+            points_object.current_location.latitude = ChangeToRadian(points_object.current_location.latitude); 
 
-function ConvertObject(points_object)
-{
-    points_object.current_location.longtitude = ChangeToRadian(points_object.current_location.longtitude); 
-    points_object.current_location.latitude = ChangeToRadian(points_object.current_location.latitude); 
+            points_object.destination.longtitude = ChangeToRadian(points_object.destination.longtitude); 
+            points_object.destination.latitude = ChangeToRadian(points_object.destination.latitude); 
 
-    points_object.destination.longtitude = ChangeToRadian(points_object.destination.longtitude); 
-    points_object.destination.latitude = ChangeToRadian(points_object.destination.latitude); 
-
-    return points_object; 
-}
-
-function ChangeToRadian(number)
-{
-    return Math.PI * number / 180; 
+            return points_object; 
+        }, 
+        CalculateDistance(points_object)
+        {
+            const earth_radius = 6378; 
+            return earth_radius * 
+                Math.acos
+                (
+                    Math.sin(points_object.current_location.latitude) * Math.sin(points_object.destination.latitude) + Math.cos(points_object.current_location.latitude) * Math.cos(points_object.destination.latitude) * Math.cos(points_object.destination.longtitude - points_object.current_location.longtitude)
+                ); 
+        }
+    }
+    points_object = support_functions.ConvertObject(points_object); 
+    return support_functions.CalculateDistance(points_object); 
 }
