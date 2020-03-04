cde1c24b6d7256f2d7bbe2e98b423db313fc75f4
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-05 12:55:53 +1300

    Update map_actions.js

--- map_actions.js
+++ map_actions.js
@@ -1,8 +1,16 @@
 function TrackMyLocation(button)
 {
+    var CurrentLocationGraphic = function(uid)
+    {
+        for(var graphic of window.map_view.graphics.items)
+        {
+            console.log(graphic); 
+        }
+    }
     var tree = $(button).attr("data-tree"); 
     tree = JSON.parse(tree); 
     console.log(tree); 
+    CurrentLocationGraphic(tree.uid); 
     var track_my_location = navigator.geolocation.watchPosition
     (
         function(location)
