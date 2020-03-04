be3189f4012a9acd901ca04d38f902cc24629304
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-05 10:27:13 +1300

    Update direction.js

--- direction.js
+++ direction.js
@@ -2,95 +2,93 @@ function Directions(destination_graphic)
 {
     var support_functions = 
     {
-        DrawDirectionOnMap(current_location_point)
+        PutRelevantPoints(current_location_point)
         {
-            var support_functions = 
-            {
-                CreateGraphic(point)
-                {
-                    return new window.ArcGis.Graphic 
-                    (
-                        {
-                            geometry: point, 
-                            symbol: 
-                            {
-                                type: "simple-marker",
-                                color: "blue",  
-                                size: "8px"
-                            }
-                        }
-                    ); 
-                }, 
-                RemoveIrrelevantPoints(points)
-                {
-                    window.map_view.graphics.removeAll();
-                    points.forEach
-                    (
-                        point => 
-                        {
-                            window.map_view.graphics.add(point); 
-                        }
-                    );
-                }, 
-                GetDirection(points)
-                {
-                    var routeTask = new window.ArcGis.RouteTask
-                    (
-                        {
-                            url: "https://utility.arcgis.com/usrsvcs/appservices/AVA7HfDc1IGamElH/rest/services/World/Route/NAServer/Route_World/solve"
-                        }
-                    );
-        
-                    var routeParams = new window.ArcGis.RouteParameters
-                    (
-                        {
-                            stops: new window.ArcGis.FeatureSet
-                            (
-                                {
-                                    features: points
-                                }
-                            ),
-                            returnDirections: true
-                        }
-                    );
-        
-                    routeTask.solve(routeParams)
-                    .then
-                    (
-                        function(data)
-                        {
-                            data.routeResults.forEach
-                            (
-                                function(direction)
-                                {
-                                    direction.route.symbol = 
-                                    {
-                                        type: "simple-line",
-                                        color: "blue",
-                                        width: 3
-                                    }
-                                    window.map_view.graphics.add(direction.route); 
-                                }
-                            ); 
-                        }
-                    ); 
-                }
-            }
             return new Promise
             (
                 (resolve,reject)=>
                 {
-                    console.log("this runs after the other promise resolve, after finding the point destination"); 
+                    var support_functions = 
+                    {
+                        CreateGraphic(point)
+                        {
+                            return new window.ArcGis.Graphic 
+                            (
+                                {
+                                    geometry: point, 
+                                    symbol: 
+                                    {
+                                        type: "simple-marker",
+                                        color: "blue",  
+                                        size: "8px"
+                                    }
+                                }
+                            ); 
+                        }, 
+                        RemoveIrrelevantPoints(points)
+                        {
+                            window.map_view.graphics.removeAll();
+                            points.forEach
+                            (
+                                point => 
+                                {
+                                    window.map_view.graphics.add(point); 
+                                }
+                            );
+                            resolve(points); 
+                        }
+                    }
                     var current_location_graphic = support_functions.CreateGraphic(current_location_point); 
                     var points = [current_location_graphic, destination_graphic]; 
                     support_functions.RemoveIrrelevantPoints(points); 
-                    support_functions.GetDirection(points); 
-                    resolve(); 
+                }
+            );  
+        }, 
+        GetDirection(points)
+        {
+            var routeTask = new window.ArcGis.RouteTask
+            (
+                {
+                    url: "https://utility.arcgis.com/usrsvcs/appservices/AVA7HfDc1IGamElH/rest/services/World/Route/NAServer/Route_World/solve"
+                }
+            );
+
+            var routeParams = new window.ArcGis.RouteParameters
+            (
+                {
+                    stops: new window.ArcGis.FeatureSet
+                    (
+                        {
+                            features: points
+                        }
+                    ),
+                    returnDirections: true
+                }
+            );
+
+            routeTask.solve(routeParams)
+            .then
+            (
+                function(data)
+                {
+                    data.routeResults.forEach
+                    (
+                        function(direction)
+                        {
+                            direction.route.symbol = 
+                            {
+                                type: "simple-line",
+                                color: "blue",
+                                width: 3
+                            }
+                            window.map_view.graphics.add(direction.route); 
+                        }
+                    ); 
                 }
             ); 
         }
     }
-    window.dectect_current_location.then(support_functions.DrawDirectionOnMap); 
+    window.dectect_current_location.then(support_functions.PutRelevantPoints).then(GetDirection); 
 }
 
 
@@ -107,8 +105,6 @@ function FindDirectionToPoint()
                 (
                     (resolve, reject)=>
                     {
-                        console.log(response); 
-                        console.log("this run after the hit test"); 
                         for (let index = 0; index < response.results.length; index++) 
                         {
                             let graphic = response.results[index].graphic; 
