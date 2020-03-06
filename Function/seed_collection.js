99f1f5f2c019182e8e0908b1cf7c3eeb58a65ec9
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-06 15:57:43 +1300

    Put all in learn component

--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -1,5 +1,5 @@
-import {Li} from "./SeedCollection/list_item_component.js"; 
-import {Information} from "./SeedCollection/information_component.js"; 
+import {Li} from "./SeedCollection/LearnComponent/list_item_component.js"; 
+import {Information} from "./SeedCollection/LearnComponent/information_component.js/index.js"; 
 
 
 var seed_collection = new Vue 
