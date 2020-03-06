9d110451992a9c6a85adb4e025fed2fa6c56a9d7
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-06 13:23:32 +1300

    Update seed_collection.js

--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -10,10 +10,23 @@ var seed_collection = new Vue
         {
             title: "Seed Collection", 
             menu: ["Collect","Store"], 
+            current_tab: "Collect", 
             tree: {}
         }, 
         methods: 
         {
+            ButtonClass(item)
+            {
+                let btn_class = ["btn", "btn-light", "col-" + Math.floor(12/this.menu.length)]; 
+                if(this.current_tab==item)
+                {
+                    btn_class.push("active"); 
+                }
+                return btn_class; 
+            },
+
+
+            //get rid of this at the end
             GiveMeAFakeTree: function()
             {
                 this.tree = 
