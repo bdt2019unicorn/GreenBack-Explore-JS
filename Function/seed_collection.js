3e3e4963aa8c994aa1eb5209828b0b46d7249285
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-06 13:06:05 +1300

    Update seed_collection.js

--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -10,6 +10,29 @@ var seed_collection = new Vue
         {
             title: "Seed Collection", 
             tree: {}
-        }
+        }, 
+        methods: 
+        {
+            GiveMeAFakeTree()
+            {
+                this.tree = 
+                {
+                    name: "Tree 2", 
+                    type: "Kauri", 
+                    store: 
+                    `
+                        Do a
+                        Do b
+                        Do c
+                    `,
+                    collect: 
+                    `
+                        collect 1
+                        collect 2
+                        collect 3
+                    `
+                }
+            }    
+        },
     }
 ); 
