98aea5d55a480c375d9ac74ed01ec9477b9adf46
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-07 23:57:53 +1300

    Update learn_component.js

--- Function/SeedCollection/learn_component.js
+++ Function/SeedCollection/learn_component.js
@@ -2,7 +2,7 @@ export var Learn = Vue.component
 (
     "LearnComponent", 
     {
-        props: ["tree", "current_tab", "db_top_key", "db_key_title", "tree"], 
+        props: ["tree", "current_tab", "db_top_key", "db_key_title", "menu"], 
         template:
         `
 
@@ -15,7 +15,7 @@ export var Learn = Vue.component
                     @{{item}}
                 </button>
             </div>
-            
+
             <Information :data="tree[db_top_key+current_tab.toLowerCase()]">
             </Information>
 
