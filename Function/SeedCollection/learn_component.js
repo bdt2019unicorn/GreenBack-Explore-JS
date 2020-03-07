5cebb6d00708057ae965d64710d639d0e487f6be
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-07 23:53:29 +1300

    Update learn_component.js

--- Function/SeedCollection/learn_component.js
+++ Function/SeedCollection/learn_component.js
@@ -5,9 +5,32 @@ export var Learn = Vue.component
         props: ["tree", "current_tab", "db_top_key", "db_key_title", "tree"], 
         template:
         `
+
+            <div class="row">
+                <button 
+                    v-for="item in menu" 
+                    :class='ButtonClass(item)'
+                    @click='current_tab=item'
+                >
+                    @{{item}}
+                </button>
+            </div>
+            
             <Information :data="tree[db_top_key+current_tab.toLowerCase()]">
             </Information>
 
-        `
+        `, 
+        methods: 
+        {
+            ButtonClass(item)
+            {
+                let btn_class = ["btn", "btn-light", "col-" + Math.floor(12/this.menu.length)]; 
+                if(this.current_tab==item)
+                {
+                    btn_class.push("active"); 
+                }
+                return btn_class; 
+            },
+        },
     }
 ); 
