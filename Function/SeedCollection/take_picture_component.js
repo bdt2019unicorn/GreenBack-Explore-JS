f7146d0210ba6aec2a42dfb757cb88bed9ab2e89
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-10 16:47:02 +1300

    Update take_picture_component.js

--- Function/SeedCollection/take_picture_component.js
+++ Function/SeedCollection/take_picture_component.js
@@ -14,33 +14,31 @@ export var TakePictureComponent = Vue.component
         },
         template: 
         `
-            <div class="full-width full-page take-picture">
 
-                <div class="full-width full-page card bg-secondary">
+            <div class="full-width full-page card bg-secondary">
 
-                    <div class="card-header">
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-6" align="left">
-                                    <button class="btn btn-primary">X</button>
-                                </div>
-            
-                                <div class="col-6" align="right">
-                                    <button class="btn btn-link" hidden>V</button>
-                                </div>
+                <div class="card-header">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-6" align="left">
+                                <button class="btn btn-primary">X</button>
+                            </div>
+        
+                            <div class="col-6" align="right">
+                                <button class="btn btn-link">V</button>
                             </div>
                         </div>
                     </div>
-
-                    <div class="card-body">
-                        Content
-                    </div>
-
-                    <div class="card-footer">
-                        Footer
-                    </div>
-
                 </div>
+
+                <div class="card-body">
+                    Content
+                </div>
+
+                <div class="card-footer">
+                    Footer
+                </div>
+
             </div>
 
         `,
