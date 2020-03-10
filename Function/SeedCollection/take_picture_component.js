1b43c0c71e9aa4bc3bcea83ca2988190be560656
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-10 18:18:48 +1300

    Update take_picture_component.js

--- Function/SeedCollection/take_picture_component.js
+++ Function/SeedCollection/take_picture_component.js
@@ -50,6 +50,16 @@ export var TakePictureComponent = Vue.component
                             <div class="col-2"></div>
                         </div>
 
+                        <div class="row">
+
+                            <div class="col-2"></div>
+                            <div class="col" align="center">
+                                <p>When shooting, be sure </p>
+                                <p>the subject’s image is in focus</p>
+                            </div>
+                            <div class="col-2"></div>
+                        </div>
+
                     </div>
                 </div>
 
