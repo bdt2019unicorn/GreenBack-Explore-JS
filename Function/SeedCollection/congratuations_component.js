93f6285bd742d77fcda405364181134b20dabcf5
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-10 19:11:44 +1300

    congrat component finished

---
+++ Function/SeedCollection/congratuations_component.js
@@ -0,0 +1,23 @@
+export var CongratulationsComponent = Vue.component
+(
+    "CongratulationsComponent", 
+    {
+        template: 
+        `
+
+            <div class="card bg-light">
+
+                <div class="card-header"></div>
+                <div class="card-body"></div>
+                <div class="card-footer" align="right">   
+                    <button class="btn"><i class="fas fa-times-circle"></i></button>
+                </div>
+
+            </div>
+
+        `,
+        methods: 
+        { 
+        },
+    }, 
+); 
--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -1,7 +1,7 @@
 import * as learn_component from "./SeedCollection/learn_component.js"; 
 import * as term_condition_component from "./SeedCollection/term_condition_component.js"; 
 import * as take_picture_component from "./SeedCollection/take_picture_component.js"; 
-
+import * as congratuations_component from "./SeedCollection/congratuations_component.js"; 
 
 var seed_collection = new Vue 
 (
