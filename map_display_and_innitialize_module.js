4457958aa28654daa386246331a6c82997f7468b
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-12 12:58:36 +1300

    Update map_display_and_innitialize_module.js

--- map_display_and_innitialize_module.js
+++ map_display_and_innitialize_module.js
@@ -1,31 +1,6 @@
 import {InnitizeMap} from "./Map Module/innitialize_map.js"; 
 import {AddLocationWidget} from "./Map Module/add_location_widget.js"; 
 
-// InnitizeMap.then(DetectCurrentLocation).then 
-// (
-//     function(point)
-//     {
-//         window.map_view.center = point;
-//         var location_widget = AddLocationWidget(window.ArcGis.Locate, window.map_view); 
-//         window.map_view.when 
-//         (
-//             function()
-//             {
-//                 location_widget.locate(); 
-//             }
-//         ); 
-//         return new Promise 
-//         (
-//             (resolve, reject)=>
-//             {
-//                 GetDataAroundLocation();
-//                 FindDirectionToPoint(); 
-//                 resolve(); 
-//             }
-//         );
-//     }
-// ); 
-
 
 InnitizeMap.then 
 (
