180c4c4ea0718911a04e7744e44239d2c8777990
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-12 12:32:34 +1300

    Update map_display_and_innitialize_module.js

--- map_display_and_innitialize_module.js
+++ map_display_and_innitialize_module.js
@@ -1,35 +1,51 @@
 import {InnitizeMap} from "./Map Module/innitialize_map.js"; 
 import {AddLocationWidget} from "./Map Module/add_location_widget.js"; 
 
-InnitizeMap.then(DetectCurrentLocation).then 
+// InnitizeMap.then(DetectCurrentLocation).then 
+// (
+//     function(point)
+//     {
+//         window.map_view.center = point;
+//         var location_widget = AddLocationWidget(window.ArcGis.Locate, window.map_view); 
+//         window.map_view.when 
+//         (
+//             function()
+//             {
+//                 location_widget.locate(); 
+//             }
+//         ); 
+//         return new Promise 
+//         (
+//             (resolve, reject)=>
+//             {
+//                 GetDataAroundLocation();
+//                 FindDirectionToPoint(); 
+//                 resolve(); 
+//             }
+//         );
+//     }
+// ); 
+
+
+InnitizeMap.then 
 (
-    function(point)
+    function()
     {
-        window.map_view.center = point;
         var location_widget = AddLocationWidget(window.ArcGis.Locate, window.map_view); 
+        window.map_view.when 
+        (
+            function()
+            {
+                location_widget.locate(); 
+            }
+        ); 
         return new Promise 
         (
             (resolve, reject)=>
             {
                 GetDataAroundLocation();
                 FindDirectionToPoint(); 
-                resolve(location_widget); 
-            }
-        ).then 
-        (
-            function(location_widget)
-            {
-                window.map_view.when 
-                (
-                    function()
-                    {
-                        location_widget.locate(); 
-                    }
-                ); 
-                // console.log(location_widget); 
-                // console.log("I should be working now"); 
-                // return location_widget.locate(); 
-                // window.location_widget = location_widget; 
+                resolve(); 
             }
         );
     }
