732a85c2239dea97de2b32fd20666edb945a48cb
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-11 16:03:28 +1300

    Update term_condition_component.js

--- Function/SeedCollection/term_condition_component.js
+++ Function/SeedCollection/term_condition_component.js
@@ -12,7 +12,7 @@ var TermConditionComponent = Vue.component
         {  
             return {
                 terms: term_condition_data, 
-                count: 0,
+                count: window.term_condition_store_track.getState(),
                 accept_button: false 
             };
         },
