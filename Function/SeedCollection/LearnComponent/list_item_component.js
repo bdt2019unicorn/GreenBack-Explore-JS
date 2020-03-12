267138b7d4b7e04c3a3129b646893473b20fa6e6
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-13 12:22:33 +1300

    Update list_item_component.js

--- Function/SeedCollection/LearnComponent/list_item_component.js
+++ Function/SeedCollection/LearnComponent/list_item_component.js
@@ -7,11 +7,11 @@ export var ListItem = Vue.component
         `
             <li class="list-group-item borderless">
 
-                <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex justify-content-between align-items-start">
 
-                    <i class="fas fa-lightbulb"></i>
-                    <br>
-                    <div class="bg-secondary round">
+                    <i class="fas fa-lightbulb flex-grow-1"></i>
+                    <br class="flex-grow-1">
+                    <div class="bg-secondary rounded flex-grow-3">
                         <p class="text-white">
                             {{value}}
                         </p>
