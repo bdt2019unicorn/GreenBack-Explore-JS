438f0532d0f47d049a917b141fb6c156d48761bb
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-13 00:30:24 +1300

    change fake tree

--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -23,45 +23,7 @@ window.seed_collection = new Vue
             },
             current_component: "LearnComponent", 
             tree: {}
-        }, 
-        methods: 
-        {
-            //get rid of this at the end
-            GiveMeAFakeTree: function()
-            {
-                this.tree = 
-                {
-                    name: "Tree 2", 
-                    type: "Kauri", 
-                    store: 
-                    `
-                        Do a
-                        Do b
-                        Do c
-                    `,
-                    collect: 
-                    `
-                        collect 1
-                        collect 2
-                        collect 3
-                    `, 
-                    tips_store: 
-                    `
-                        tips store 1
-                        tips store 2 
-                        tips store 3
-                        tips store 4
-                    `,
-                    tips_collect: 
-                    `
-                        tips collect 1
-                        tips collect 2
-                        tips collect 3
-                        tips collect 4
-                    `
-                }
-            }    
-        }, 
+        },
         watch: 
         {
             tree()
--- map_user_actions_functions.js
+++ map_user_actions_functions.js
@@ -71,3 +71,38 @@ function ChangeTipsAndCollection(button)
     let new_index = (attributes.indexOf(db_top_key) + 1)%2; 
     button.setAttribute("data-db_top_key",attributes[new_index]); 
 }
+
+function GiveMeAFakeTree()
+{
+    window.seed_collection.tree = 
+    {
+        name: "Tree 2", 
+        type: "Kauri", 
+        store: 
+        `
+            Do a
+            Do b
+            Do c
+        `,
+        collect: 
+        `
+            collect 1
+            collect 2
+            collect 3
+        `, 
+        tips_store: 
+        `
+            tips store 1
+            tips store 2 
+            tips store 3
+            tips store 4
+        `,
+        tips_collect: 
+        `
+            tips collect 1
+            tips collect 2
+            tips collect 3
+            tips collect 4
+        `
+    }; 
+}
