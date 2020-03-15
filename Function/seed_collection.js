ecb3c02c601ba17f1d982d1beba0447c6f736bb0
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-15 15:36:36 +1300

    Update seed_collection.js

--- Function/seed_collection.js
+++ Function/seed_collection.js
@@ -5,45 +5,45 @@ import * as congratuations_component from "./SeedCollection/congratuations_compo
 
 window.term_condition_store_track = term_condition_component.CreateStoreTrack();
 
-window.seed_collection = new Vue 
-(
-    {
-        el: "#seed_collection", 
-        data: 
-        {
-            menu: ["Collect","Store"], 
-            db_top_key: "", 
-            db_key_title: 
-            {
-                "": "Collection and Storage", 
-                "tips_": "Tips", 
-                "term_condition": "",
-                "congratuations": "CONGRATUATIONS!"
-            },
-            class_lists: 
-            {
-                "LearnComponent": "common-popup main-components learn-term-components learn-component", 
-                "TermConditionComponent": "common-popup main-components learn-term-components term-component",
-                "TakePictureComponent": "common-popup main-components picture-component", 
-                "CongratulationsComponent": "common-popup congrat-component"
+// window.seed_collection = new Vue 
+// (
+//     {
+//         el: "#seed_collection", 
+//         data: 
+//         {
+//             menu: ["Collect","Store"], 
+//             db_top_key: "", 
+//             db_key_title: 
+//             {
+//                 "": "Collection and Storage", 
+//                 "tips_": "Tips", 
+//                 "term_condition": "",
+//                 "congratuations": "CONGRATUATIONS!"
+//             },
+//             class_lists: 
+//             {
+//                 "LearnComponent": "common-popup main-components learn-term-components learn-component", 
+//                 "TermConditionComponent": "common-popup main-components learn-term-components term-component",
+//                 "TakePictureComponent": "common-popup main-components picture-component", 
+//                 "CongratulationsComponent": "common-popup congrat-component"
 
-            }, 
-            current_component: "LearnComponent", 
-            tree: {}
-        },
-        watch: 
-        {
-            tree()
-            {
-                try 
-                {
-                    this.db_key_title["term_condition"] = "Warning before collecting " + this.tree.type + " cones!"; 
-                }
-                catch
-                {
-                    this.db_key_title["term_condition"] = ""; 
-                }
-            }    
-        },
-    }
-); 
+//             }, 
+//             current_component: "LearnComponent", 
+//             tree: {}
+//         },
+//         watch: 
+//         {
+//             tree()
+//             {
+//                 try 
+//                 {
+//                     this.db_key_title["term_condition"] = "Warning before collecting " + this.tree.type + " cones!"; 
+//                 }
+//                 catch
+//                 {
+//                     this.db_key_title["term_condition"] = ""; 
+//                 }
+//             }    
+//         },
+//     }
+// ); 
