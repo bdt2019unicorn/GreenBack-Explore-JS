e2568e8e96f8c93ec9719456aebed75ae84cf06d
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-15 15:38:55 +1300

    fix button controller

--- Function/SeedCollectionController/button_controller.js
+++ Function/SeedCollectionController/button_controller.js
@@ -2,7 +2,7 @@ export var ButtonController = Vue.component
 (
     "ButtonController", 
     {
-        props: ["name", "subset","db_top_key", "current_component", "onclick"],
+        props: ["controller_name", "subset","db_top_key", "current_component", "onclick"],
         data: function() 
         {  
             return {
@@ -15,8 +15,8 @@ export var ButtonController = Vue.component
             <input 
                 type="image" 
                 :onclick="onclick"
-                :src="this.base_url +subset + name+'.png'" 
-                :class="'common-popup '+ name"
+                :src="this.base_url +subset + controller_name+'.png'" 
+                :class="'common-popup '+ controller_name"
                 :data-db_top_key="db_top_key"
                 :data-current_component="current_component"
             >
